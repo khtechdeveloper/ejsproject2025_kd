896fb111ce53e1be24ae4f490707cc17342bde59
khtechdeveloper <khtechdeveloper@gmail.com>
2025-09-04 11:57:43 +0530

    added mother name field

--- models/Student.js
+++ models/Student.js
@@ -6,6 +6,7 @@ const studentSchema = new Schema({
      rollNo: { type: String, required: true },
      studentName: { type: String, required: true },
      fatherName: { type: String, required: true },
+     motherName: {type : String, required: true},
      course: { type: String, required: true },
      branch: { type: String, },
      yearOfAdmission: { type: String, },
